e446e8ff0407c4fc4a765d93e10665b3258c354b
matheo-prd <perod.matheo@gmail.com>
2025-03-15 17:33:16 +0100

    Nouvelle structure du Bot en TypeScript

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { Client, Collection, GatewayIntentBits } from "discord.js";
+import * as dotenv from "dotenv";
+import { readdirSync } from "fs";
+import { join } from "path";
+import { SlashCommand } from "./types";
+
+dotenv.config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+
+try {
+    client.slashCommands = new Collection<string, SlashCommand>();
+
+    const handlersDirs = join(__dirname, "./handlers/discord");
+
+    readdirSync(handlersDirs).forEach(file => {
+        require(`${handlersDirs}/${file}`)(client)
+    })
+
+} catch (error) {
+    console.error("Erreur lors de l'initialisation des commandes slash :", error);
+}
+
+client.login(process.env.DISCORD_TOKEN);
+
