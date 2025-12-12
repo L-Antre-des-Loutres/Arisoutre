5940707fa5ca2ee6c91863ea6c9522f96ceedef3
matheo-1712 <perod.matheo@gmail.com>
2025-12-12 14:00:47 +0100

    Ajoute la gestion des erreurs et des logs pour la commande `/link-account`, avec réponse utilisateur améliorée pour les cas d'échec.

--- src/app/commands/link-account.ts
+++ src/app/commands/link-account.ts
@@ -6,6 +6,7 @@ import {
 } from "discord.js";
 import {Otterlyapi} from "../../otterbots/utils/otterlyapi/otterlyapi";
 import {UtilisateursDiscordType} from "../types/UtilisateursDiscordType";
+import {otterlogs} from "../../otterbots/utils/otterlogs";
 
 export default {
     data: new SlashCommandBuilder()
@@ -39,34 +40,43 @@ export default {
             return;
         }
 
-        await interaction.deferReply(); // pour gérer les réponses pouvant être un peu longues
+        try {
+            await interaction.deferReply({flags: "Ephemeral"}); // pour gérer les réponses pouvant être un peu longues
 
-        // On récupére les informations de l'utilisateur Discord
-        const utilisateur_infos = await Otterlyapi.getDataByAlias<UtilisateursDiscordType>("otr-utilisateursDiscord-getByDiscordId", member.id)
-        
-        if (!utilisateur_infos) {
+            // On récupére les informations de l'utilisateur Discord
+            const utilisateur_infos = await Otterlyapi.getDataByAlias<UtilisateursDiscordType>("otr-utilisateursDiscord-getByDiscordId", member.id)
+
+            if (!utilisateur_infos) {
+                await interaction.editReply({
+                    content: "Impossible de trouver votre compte sur le serveur.",
+                });
+                return;
+            }
+
+            // On renvoie le code et l'id de l'utilisateur
+            const response = await Otterlyapi.postDataByAlias(
+                "otr-joueurs-link-account",
+                { code, utilisateur_id: utilisateur_infos.id }
+            );
+
+            if (!response) {
+                await interaction.editReply({
+                    content: "Le code de liaison est invalide ou a déjà été utilisé.",
+                });
+                return;
+            }
+
             await interaction.editReply({
-                content: "Impossible de trouver votre compte sur le serveur.",
+                content: "La liaison avec votre compte a bien été effectué.",
             });
-            return;
+        } catch (error: unknown) {
+            otterlogs.error("Erreur lors de la liaison du compte : " + error)
+            await interaction.editReply({
+                content: "Une erreur est survenue lors de la liaison du compte merci de contacter un administrateur."
+            })
         }
 
-        // On renvoie le code et l'id de l'utilisateur
-        const response = await Otterlyapi.postDataByAlias(
-            "otr-joueurs-link-account",
-            { code, utilisateur_id: utilisateur_infos.id }
-        );
 
-        if (!response) {
-            await interaction.editReply({
-                content: "Le code de liaison est invalide ou a déjà été utilisé.",
-            });
-            return;
-        }
-
-        await interaction.editReply({
-            content: "Votre compte a bien été lié",
-        });
 
 
 
