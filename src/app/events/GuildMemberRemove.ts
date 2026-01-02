af2ddc89b7c8a4f81d8d331a6b2e2986fd08958a
matheo-1712 <perod.matheo@gmail.com>
2026-01-03 00:10:56 +0100

    Ajoute des commentaires de documentation pour `exportOldDiscordStats`, `getSqlDate`, et améliore la gestion des rôles dans `loutreAssureCheck` en ajoutant la récupération du client Discord et des conditions supplémentaires pour les utilisateurs.

--- src/app/events/GuildMemberRemove.ts
+++ src/app/events/GuildMemberRemove.ts
@@ -13,7 +13,6 @@ module.exports = {
         const guild = member.guild
 
         try {
-
             // On ne fait rien si le membre est un bot
             if (member.user.bot) return
 
@@ -25,14 +24,34 @@ module.exports = {
                 await guild.channels.fetch(channelModeratorId) as TextChannel;
             if (!channel) return otterlogs.error('Unable to retrieve moderator message channel');
 
-            const userInfo: UtilisateursDiscordType | undefined = await Otterlyapi.getDataByAlias("otr-utilisateursDiscord-getByDiscordId", member.user.id)
+            let userInfo: UtilisateursDiscordType | undefined = await Otterlyapi.getDataByAlias("otr-utilisateursDiscord-getByDiscordId", member.user.id)
 
-            if (userInfo) await Otterlyapi.putDataByAlias("otr-utilisateursDiscord-updateDataSuppressionDate", {discord_id: member.user.id})
+
+            if (userInfo) {
+                await Otterlyapi.putDataByAlias("otr-utilisateursDiscord-updateDataSuppressionDate", {discord_id: member.user.id});
+            } else {
+                // On génére des données minimales pour l'utilisateur afin d'avoir un historique de sa suppression
+                await Otterlyapi.putDataByAlias("otr-utilisateursDiscord-updateDataSuppressionDate", {
+                    avatar_url: member.user.displayAvatarURL(),
+                    delete_date: new Date().toISOString(),
+                    first_activity: new Date().toISOString(),
+                    id: 0,
+                    last_activity: new Date().toISOString(),
+                    nb_message: 0,
+                    roles: [],
+                    tag_discord: member.user.tag,
+                    vocal_time: 0,
+                    discord_id: member.user.id,
+                    pseudo_discord: member.user.username,
+                    join_date_discord: member.joinedAt ? member.joinedAt.toISOString() : undefined
+                });
+                userInfo = await Otterlyapi.getDataByAlias("otr-utilisateursDiscord-getByDiscordId", member.user.id);
+            }
 
             await channel.send({embeds: [await embed_guildMemberRemove(userInfo)]})
 
         } catch (error) {
-            otterlogs.error('Error while sending welcome message: ' + error)
+            otterlogs.error('Error while sending leave message: ' + error)
         }
     },
 }
--- src/app/types/UtilisateursDiscordType.ts
+++ src/app/types/UtilisateursDiscordType.ts
@@ -1,10 +1,31 @@
+/**
+ * Represents a Discord user's detailed information.
+ *
+ * This type is structured to store and manage various attributes
+ * related to a Discord user, including their identification, activity,
+ * messages, roles, and other metadata.
+ *
+ * Fields:
+ * - `id`: A unique numeric identifier for the user in the system.
+ * - `discord_id`: The user's Discord ID as a string.
+ * - `pseudo_discord`: The user's Discord pseudonym or username without discriminator.
+ * - `join_date_discord`: The date the user joined the Discord server, if available.
+ * - `first_activity`: The timestamp of the user's first recorded activity, if applicable (optional).
+ * - `last_activity`: The timestamp of the user's last recorded activity, if applicable (optional).
+ * - `nb_message`: The total number of messages sent by the user in the server.
+ * - `tag_discord`: The user's complete Discord tag, including username and discriminator (e.g., "username#1234").
+ * - `avatar_url`: URL of the Discord avatar image associated with the user.
+ * - `vocal_time`: Total time the user has spent in voice channels, represented as a number.
+ * - `roles`: An array of roles associated with the user. Each role is represented as a `Roles` object.
+ * - `delete_date`: The date when the user's data was deleted, or null if the user is still active.
+ */
 export type UtilisateursDiscordType = {
     id: number
     discord_id: string
     pseudo_discord: string
-    join_date_discord: string
-    first_activity: string
-    last_activity: string
+    join_date_discord: string | undefined
+    first_activity?: string
+    last_activity?: string
     nb_message: number
     tag_discord: string
     avatar_url: string
@@ -13,6 +34,14 @@ export type UtilisateursDiscordType = {
     delete_date: string | null;
 }
 
+/**
+ * Represents a role in the system with specific attributes.
+ *
+ * @typedef {Object} Roles
+ * @property {string} id - The unique identifier for the role.
+ * @property {string} name - The display name of the role.
+ * @property {string} color - The color associated with the role, typically represented as a string.
+ */
 export type Roles = {
     id: string;
     name: string;
